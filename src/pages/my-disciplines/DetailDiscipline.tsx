978eb0d20519abafe94de46d40b0643c472a7091
Sergey-hub02 <s.park190802@gmail.com>
2026-05-18 21:33:09 +0300

    Немного доделал страницу детального просмотра дисциплины.Добавил отображение преподавателей по дисциплине

--- src/pages/my-disciplines/DetailDiscipline.tsx
+++ src/pages/my-disciplines/DetailDiscipline.tsx
@@ -1,18 +1,51 @@
+import { useEffect, useState } from 'react'
 import { useLoaderData } from 'react-router'
-import { Container, Row, Col, Image, ListGroupItem } from 'react-bootstrap'
+import { Container, Row, Col, Image, ListGroupItem, Alert } from 'react-bootstrap'
 
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
 import TableOfContents from '@/components/TableOfContents/TableOfContents'
 
-import disciplines from '@/mocks/disciplines'
 import './DetailDiscipline.scss'
 
 const DetailDiscipline = () => {
     const { disciplineId } = useLoaderData()
+    const [discipline, setDiscipline] = useState<any>()
+    const [error, setError] = useState<string>('')
+    const [loading, setLoading] = useState<boolean>(true)
 
-    const [discipline] = disciplines.filter(disc => disc.guid === disciplineId)
-    const description = discipline.fullDescription ?? discipline.description
+    const fetchDisciplineData = async (discGuid: string) => {
+        const response = await fetch(`/api/v1/disciplines/${discGuid}`)
+        const body = await response.json()
+
+        if (!response.ok && body?.error) {
+            setError(body.error)
+            setLoading(false)
+            return
+        }
+
+        setError('')
+        setDiscipline(body)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchDisciplineData(disciplineId)
+    }, [])
+
+    if (loading) {
+        return (
+            <div className="d-flex flex-column min-vh-100">Загрузка...</div>
+        )
+    }
+
+    if (!loading && error) {
+        return (
+            <div className="d-flex flex-column min-vh-100">
+                <Alert variant="danger">{error}</Alert>
+            </div>
+        )
+    }
 
     return (
         <div className="d-flex flex-column min-vh-100">
@@ -42,8 +75,15 @@ const DetailDiscipline = () => {
                                 <div className="section-content">
                                     <div>
                                         <h5>Описание дисциплины</h5>
-                                        <div className="section-description">{description}</div>
-                                        <div><strong>Дисциплину ведут:</strong> {discipline.teachers}</div>
+                                        <div className="section-description mb-2">{discipline.description}</div>
+
+                                        {discipline.teachers && discipline.teachers.length > 0 && (
+                                            <div><strong>Дисциплину ведут:</strong> {discipline.teachers}</div>
+                                        )}
+
+                                        {discipline.tests?.length > 0 && (
+                                            <div><strong>Количество тестов:</strong> {discipline.tests.length}</div>
+                                        )}
                                     </div>
                                 </div>
                             </section>
@@ -54,7 +94,7 @@ const DetailDiscipline = () => {
                                 <TableOfContents title={`Тесты по дисциплине "${discipline.name}"`}>
                                     {discipline.tests
                                         && discipline.tests.length > 0
-                                        && discipline.tests.map(test => (
+                                        && discipline.tests.map((test: any) => (
                                             <ListGroupItem
                                                 key={test.guid}
                                                 href={`./tests/${test.guid}`}
